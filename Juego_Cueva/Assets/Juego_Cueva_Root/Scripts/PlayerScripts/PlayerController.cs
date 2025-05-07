02358c7c1dabdc7c3bc613540ff134408b03e127
Jorgegiral <jorgegiraldo758@gmail.com>
2025-05-07 20:15:20 +0200

    Player

--- Juego_Cueva/Assets/Juego_Cueva_Root/Scripts/PlayerScripts/PlayerController.cs
+++ Juego_Cueva/Assets/Juego_Cueva_Root/Scripts/PlayerScripts/PlayerController.cs
@@ -7,9 +7,11 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Movement & Look Stats")]
     [SerializeField] GameObject camHolder;
+    [SerializeField] private float rotationSpeed = 4f;
     public float speed;
     public float maxForce = 1; //Límite de aceleración máxima
     public float sensitivity = 0.1f; //Sensibilidad aplicada al input de observar
+    private Transform cameraFollowTransform;
 
     [Header("Jumping Stats")]
     public float jumpForce;
@@ -30,8 +32,9 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
-        camHolder = GameObject.Find("CameraHolder");
+        //camHolder = GameObject.Find("CameraHolder");
         groundCheck = GameObject.Find("GroundCheck");
+        cameraFollowTransform = Camera.main.transform;
     }
     void Start()
     {
@@ -41,6 +44,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        HandleAnimations();
         isGrounded = Physics.CheckSphere(groundCheck.transform.position, groundCheckRadius, groundLayer);
     }
     private void FixedUpdate()
@@ -50,16 +54,27 @@ public class PlayerController : MonoBehaviour
 
     private void LateUpdate()
     {
-        CameraLook();
+        //CameraLook();
     }
 
     void Movement()
     {
         Vector3 currentVelocity = playerRb.velocity; //Velocidad actual del player
-        Vector3 targetVelocity = new Vector3(moveInput.x, 0, moveInput.y); //Velocidad hacia la que queremos que se mueva el player
+        /*Vector3 targetVelocity = new Vector3(moveInput.x, 0, moveInput.y); //Velocidad hacia la que queremos que se mueva el player
         targetVelocity *= speed;
         //Alinear la dirección con la orientación correcta (de local a global)
-        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity = transform.TransformDirection(targetVelocity);*/
+        Vector3 inputDir = new Vector3(moveInput.x, 0, moveInput.y);
+        Vector3 camForward = cameraFollowTransform.forward;
+        Vector3 camRight = cameraFollowTransform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        // Movimiento relativo a la cámara
+        Vector3 targetVelocity = (camForward * inputDir.z + camRight * inputDir.x) * speed;
 
         //Calcular las fuerzas que afectan al movimiento
         Vector3 velocityChange = (targetVelocity - currentVelocity);
@@ -69,11 +84,17 @@ public class PlayerController : MonoBehaviour
         //Aplicamos el movimiento
         playerRb.AddForce(velocityChange, ForceMode.VelocityChange);
 
-        anim.SetBool("isRunning", Mathf.Abs(moveInput.x) > 0.1f || Mathf.Abs(moveInput.y) > 0.1f);
+        if (moveInput != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(moveInput.x, moveInput.y) * Mathf.Rad2Deg + cameraFollowTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.fixedDeltaTime * rotationSpeed);
+        }
 
+        
     }
 
-    void CameraLook()
+    /*void CameraLook()
     {
         //Girar (Gira el personaje en horizontal)
         transform.Rotate(Vector3.up * lookInput.x * sensitivity);
@@ -81,7 +102,7 @@ public class PlayerController : MonoBehaviour
         lookRotation += (-lookInput.y * sensitivity);
         lookRotation = Mathf.Clamp(lookRotation, -90, 90); //Restringe el valor de lookRotation entre dos valores mínimo/máximo
         camHolder.transform.eulerAngles = new Vector3(lookRotation, camHolder.transform.eulerAngles.y, camHolder.transform.eulerAngles.z);
-    }
+    }*/
 
     void Jump()
     {
@@ -89,6 +110,12 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+    }
+    void HandleAnimations()
+    {
+        anim.SetBool("isJumping", !isGrounded);
+        anim.SetFloat("VelocityY", playerRb.velocity.y);
+        anim.SetBool("isRunning", Mathf.Abs(moveInput.x) > 0.1f || Mathf.Abs(moveInput.y) > 0.1f);
     }
 
     #region Input Methods
